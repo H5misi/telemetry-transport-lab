941553efe0320fda64c0df66d2f8d4c4c3ea238e
H5misi <hazm.sayed.m@gmail.com>
2026-08-19 00:18:34 +0300

    Increase timeout for long polling requests and comment out break on timeout

--- clients/long_polling.py
+++ clients/long_polling.py
@@ -4,11 +4,11 @@ url = "https://meowmetry-app.dev1.mnt.group/api/poll"
 
 while True:
     try:
-        response = requests.get(url, timeout=2)
+        response = requests.get(url, timeout=5)
 
         print("Status:", response.status_code)
         print("Response:", response.text)
     except requests.Timeout:
         print("Request timed out")
-        break
+        # break
     
